aad43c99f4da95d2bc8f510f84760e74f40bfd5c
raidsoul <thunderock01@gmail.com>
2019-02-18 14:17:42 +0700

    Setting environment

---
+++ NewClassify.py
@@ -0,0 +1,16 @@
+import cv2
+import sys
+import re
+import os
+import json
+import imutils
+import numpy as np
+import subprocess
+from imutils import contours
+from imutils.perspective import four_point_transform
+
+def main() :
+    count = 1
+    
+
+main()
